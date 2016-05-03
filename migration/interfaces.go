572e6c0a86199e4bf3e5e8bd2adab7ea1c521832
Will Charczuk <will@clotheshor.se>
2016-05-03 14:19:30 -0700

    removing unused interfaces

--- migration/interfaces.go
+++ migration/interfaces.go
@@ -10,17 +10,9 @@ import (
 // Action is a step in a migration.
 type Action func(l *log.Logger, c *spiffy.DbConnection, tx *sql.Tx, body Invocable, args ...string) error
 
-// Invocation is a function that can be run in the migration suite.
-type Invocation func(c *spiffy.DbConnection, tx *sql.Tx) error
-
 // Invocable is a thing that can be invoked.
 type Invocable interface {
 	Invoke(c *spiffy.DbConnection, tx *sql.Tx) error
-}
-
-// Loggable is a step that is loggable.
-type Loggable interface {
-	SetLogger(logger *log.Logger)
 }
 
 // Migration is an Invocable that can tested before running.
