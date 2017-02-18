170a98e308f8d356ba438d8c7f8ed6d962a4fcb5
Will Charczuk <will@blendlabs.com>
2017-02-17 18:09:56 -0800

    ???

--- event.go
+++ event.go
@@ -37,3 +37,31 @@ func NewLoggerEventListener(action LoggerEventListener) logger.EventListener {
 		action(writer, ts, eventFlag, queryBody, elapsed, err, queryLabel)
 	}
 }
+
+// NewPrintStatementListener is a helper listener.
+func NewPrintStatementListener() logger.EventListener {
+	return func(writer logger.Logger, ts logger.TimeSource, eventFlag logger.EventFlag, state ...interface{}) {
+		var queryBody = state[0].(string)
+		var elapsed = state[1].(time.Duration)
+
+		var err error
+		if len(state) > 2 && state[2] != nil {
+			err = state[2].(error)
+		}
+
+		var queryLabel string
+		if len(state) > 3 && state[3] != nil {
+			queryLabel = state[3].(string)
+		}
+
+		if len(queryLabel) > 0 {
+			logger.WriteEventf(writer, ts, eventFlag, logger.ColorLightBlack, "(%v) %s %s", elapsed, queryLabel, queryBody)
+		} else {
+			logger.WriteEventf(writer, ts, eventFlag, logger.ColorLightBlack, "(%v) %s", elapsed, queryBody)
+		}
+
+		if err != nil {
+			writer.ErrorfWithTimeSource(ts, "%s", err.Error())
+		}
+	}
+}
