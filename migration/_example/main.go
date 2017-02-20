b9cb3a188899e22f8ed8347732062387e09b95b2
Will Charczuk <will@blendlabs.com>
2017-02-20 14:20:59 -0800

    fixing example

--- migration/_example/main.go
+++ migration/_example/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	err := spiffy.SetDefaultDb(spiffy.NewDbConnectionFromEnvironment())
+	err := spiffy.InitDefault(spiffy.NewConnectionFromEnvironment())
 	if err != nil {
 		log.Fatal(err)
 	}
